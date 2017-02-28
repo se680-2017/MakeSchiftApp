ec48fc19b0479786c8660b9b86ab87a99e19c148
Eric Tapia <tapia.ea85@gmail.com>
2017-02-27 17:32:38 -0800

    Filepicker API connected to S3, simple login page

--- src/main/java/com/example/MakeSchiftApplication.java
+++ src/main/java/com/example/MakeSchiftApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MakeSchiftApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MakeSchiftApplication.class, args);
 	}
 }
