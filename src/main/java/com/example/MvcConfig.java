54e6799e78d1fc3898eb340e4c090e081dbbe730
rekmor <RekMor@Hazims-MacBook-Pro.local>
2017-03-01 21:18:09 -0800

    file picker is implimented in profile page

---
+++ src/main/java/com/example/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Eric on 2/27/2017.
+ */
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/").setViewName("index");
+
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
