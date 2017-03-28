57d41b79a8d0a60dba98b743483cd913936f1fc4
Eric Tapia <tapia.ea85@gmail.com>
2017-03-28 16:36:15 -0700

    Dynamic user file access

--- src/main/java/com/example/WebSecurityConfig.java
+++ src/main/java/com/example/WebSecurityConfig.java
@@ -18,38 +18,38 @@ import javax.sql.DataSource;
 //@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
-
-    private DataSource dataSource;
-
-    @Value("${spring.queries.users-query}")
-    private String usersQuery;
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @Autowired
+//
+//    private DataSource dataSource;
+//
+//    @Value("${spring.queries.users-query}")
+//    private String usersQuery;
 
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-//        http.csrf().disable();//delete this to get back to normal
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/", "/index", "/register")
-//                .permitAll().anyRequest().authenticated()
-//                .and()
-//                .formLogin().loginPage("/login").permitAll()
-//                .and()
-//                .logout().permitAll();
+        //http.csrf().disable();//delete this to get back to normal
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index", "/register")
+                .permitAll().anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().permitAll();
 
     }
 
     //Creates an in-memory user. Temp for now, users & pwds should go in db
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth
-                .inMemoryAuthentication()
-                .withUser("Warren").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user1").password("password").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("user2").password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user3").password("password").roles("USER");
 
     }
 
